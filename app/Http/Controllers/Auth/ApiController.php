2056b9c5448b3f540aa0647fd847f60c4418ff52
matical <maticalssh@gmail.com>
2017-11-20 02:51:58 +0800

    Initial api and auth routes

---
+++ app/Http/Controllers/Auth/ApiController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace shiraishi\Http\Controllers\Auth;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use shiraishi\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+    use Helpers;
+
+    /**
+     * Get a JWT token via given credentials.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if ($token = $this->guard()->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return $this->response->errorUnauthorized();
+    }
+
+    /**
+     * Get the current authenticated user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public function me()
+    {
+        return $this->guard()->user();
+    }
+
+    /**
+     * Log the user out (Invalidate the token)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        $this->guard()->logout();
+
+        return [
+            'message' => 'Logged out.',
+        ];
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return [
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => $this->guard()->factory()->getTTL() * 60,
+        ];
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\Guard
+     */
+    public function guard()
+    {
+        return Auth::guard('api');
+    }
+}
