f0bd6455f46e7a41156d12fea55edcd7a69efbfb
matical <maticalssh@gmail.com>
2017-11-20 18:09:40 +0800

    Move refresh out of api.auth

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,10 @@ $api->version('v1', ['namespace' => 'shiraishi\Http\Controllers'], function ($ap
     /** @var $api \Dingo\Api\Routing\Router */
 
     $api->post('login', 'Auth\ApiController@login');
+    $api->post('refresh', 'Auth\ApiController@refresh');
 
     $api->group(['middleware' => 'api.auth'], function (Router $api) {
         $api->post('logout', 'Auth\ApiController@logout');
-        $api->post('refresh', 'Auth\ApiController@refresh');
         $api->get('me', 'Auth\ApiController@me');
     });
 
