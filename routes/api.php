a5837996c28a8a10d2ee0af9de2fff95a913e2b2
matical <maticalssh@gmail.com>
2017-11-23 03:45:50 +0800

    New namespace

--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,22 @@ use Dingo\Api\Routing\Router;
 
 $api = app('api.router');
 
-$api->version('v1', ['namespace' => 'shiraishi\Http\Controllers'], function (Router $api) {
+$api->version('v1', ['namespace' => 'shiraishi\Api\Controllers'], function (Router $api) {
 
-    $api->group(['prefix' => 'auth'], function (Router $api) {
+   $api->group(['prefix' => 'auth'], function (Router $api) {
 
-        $api->post('login', 'Auth\ApiController@login');
-        $api->post('refresh', 'Auth\ApiController@refresh');
+       $api->post('login', 'Auth\ApiController@login');
+       $api->post('refresh', 'Auth\ApiController@refresh');
 
-        $api->group(['middleware' => 'api.auth'], function (Router $api) {
-            $api->post('logout', 'Auth\ApiController@logout');
-            $api->get('me', 'Auth\ApiController@me');
+       $api->group(['middleware' => 'api.auth'], function (Router $api) {
+           $api->post('logout', 'Auth\ApiController@logout');
+           $api->get('me', 'Auth\ApiController@me');
         });
 
     });
+
+   $api->group(['prefix' => 'transaction'], function (Router $api) {
+
+   });
 
 });
