8af70c6034a33c14d831a921be233b85b6411e3c
matical <maticalssh@gmail.com>
2017-12-18 03:51:53 +0800

    Replacing bootstrap-sass with materialize-next

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,44 +1,22 @@
-
-window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
 try {
+    window._ = require('lodash');
     window.$ = window.jQuery = require('jquery');
+    window.axios = require('axios');
+    window.Echo = require('laravel-echo');
 
-    require('bootstrap-sass');
-} catch (e) {}
+    require('materialize-css');
+} catch (e) {
+}
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
+/* Init Axios */
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-window.Echo = require('laravel-echo');
-
+/* Init Echo */
 if (typeof io !== 'undefined') {
     console.log('Echo initialized');
     if (window.location.href.indexOf('localhost') > -1) {
@@ -50,7 +28,7 @@ if (typeof io !== 'undefined') {
     } else {
         window.Echo = new Echo({
             broadcaster: 'socket.io',
-            host: { path: '/socket.io' },
+            host: {path: '/socket.io'},
             namespace: 'shiraishi.Events'
         });
     }
