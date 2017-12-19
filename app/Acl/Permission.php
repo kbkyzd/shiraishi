159bdcceee89c6dc6d2b62fa357f1a9dbd3feeed
matical <maticalssh@gmail.com>
2017-12-19 22:11:30 +0800

    Custom models for roles and permissions

---
+++ app/Acl/Permission.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace shiraishi\Acl;
+
+use Spatie\Permission\Models\Permission as BasePermission;
+
+/**
+ * Base Permissions model. Inherits from Spatie's package.
+ *
+ * @package App\Acl
+ */
+class Permission extends BasePermission
+{
+}
---
+++ app/Acl/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace shiraishi\Acl;
+
+use Spatie\Permission\Models\Role as BaseRole;
+
+/**
+ * Base Permissions model. Inherits from Spatie's package.
+ *
+ * @package App\Acl
+ */
+class Role extends BaseRole
+{
+}
