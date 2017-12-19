7aec87671ed64f969fc3154e49d2069313b02d60
matical <maticalssh@gmail.com>
2017-12-20 01:57:46 +0800

    Tests for full LoginController coverage

--- tests/Feature/JwtAuthenticationTest.php
+++ tests/Feature/JwtAuthenticationTest.php
@@ -10,28 +10,61 @@ class JwtAuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * @var \shiraishi\User
+     */
+    protected $user;
+
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var array
+     */
     protected $defaultHeaders = [
         'Accept' => 'application/x.shiraishi.v1+json',
     ];
 
+    /**
+     * @var array
+     */
+    private $blacklistedToken = [
+        'message'     => 'The token has been blacklisted',
+        'status_code' => 401,
+    ];
+
+    /**
+     * @var array
+     */
+    private $accessTokenStructure = [
+        'access_token',
+        'token_type',
+        'expires_in',
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->generateValidJwtToken();
+    }
+
     /** @test */
     public function it_issues_a_valid_token_when_presented_with_valid_credentials()
     {
-        $user = factory(User::class)->create();
-
         $response = $this->postJson('/api/auth/login', [
-            'email'    => $user->email,
+            'email'    => $this->user->email,
             'password' => 'changeme',
         ]);
 
         $response->assertStatus(200)
-                 ->assertJsonStructure([
-                     'access_token',
-                     'token_type',
-                     'expires_in',
-                 ]);
+                 ->assertJsonStructure($this->accessTokenStructure);
     }
 
+    /** @test */
     public function it_denies_on_invalid_credentials()
     {
         $response = $this->postJson('/api/auth/login', [
@@ -40,5 +73,64 @@ class JwtAuthenticationTest extends TestCase
         ]);
 
         $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function it_identifies_the_user()
+    {
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+                         ->getJson('/api/auth/me');
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'user' => [
+                         'id'         => $this->user->id,
+                         'name'       => $this->user->name,
+                         'email'      => $this->user->email,
+                         'created_at' => (string) $this->user->created_at,
+                         'updated_at' => (string) $this->user->updated_at,
+                     ],
+                 ]);
+    }
+
+    /** @test */
+    public function it_allows_the_token_to_be_refreshed()
+    {
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+                         ->postJson('/api/auth/refresh');
+
+        $response->assertStatus(200)
+                 ->assertJsonStructure($this->accessTokenStructure);
+    }
+
+    /** @test */
+    public function it_allows_the_token_to_be_invalidated()
+    {
+        // Testing of whether the token is still usable should be done in dusk
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+                         ->postJson('/api/auth/logout');
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'message'     => 'Token invalidated.',
+                     'status_code' => 200,
+                 ]);
+    }
+
+    /**
+     * "Log" the user in.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    protected function generateValidJwtToken()
+    {
+        $response = $this->postJson('/api/auth/login', [
+            'email'    => $this->user->email,
+            'password' => 'changeme',
+        ]);
+
+        $this->token = $response->decodeResponseJson()['access_token'];
     }
 }
