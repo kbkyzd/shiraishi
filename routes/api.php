4066f10b00011208cbe54a1d3b6c6e6a5f4fd7bc
matical <maticalssh@gmail.com>
2017-12-19 18:13:11 +0800

    Wrap login in guest middleware, more route names for others

--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,23 @@ $api->version('v1', [
         'api',
         'api.throttle',
     ],
-    'limit'   => 100,
-    'expires' => 5,
+    'limit'      => 100,
+    'expires'    => 5,
 ], function (Router $api) {
     $api->group(['prefix' => 'auth'], function (Router $api) {
-        $api->post('login', 'Auth\LoginController@login');
-        $api->post('refresh', 'Auth\LoginController@refresh');
+        $api->group(['middleware' => 'guest'], function (Router $api) {
+            $api->post('login', 'Auth\LoginController@login')
+                ->name('api.auth.login');
+        });
+
+        $api->post('refresh', 'Auth\LoginController@refresh')
+            ->name('api.auth.refresh');
 
         $api->group(['middleware' => 'api.auth'], function (Router $api) {
-            $api->post('logout', 'Auth\LoginController@logout');
-            $api->get('me', 'Auth\LoginController@me');
+            $api->post('logout', 'Auth\LoginController@logout')
+                ->name('api.auth.logout');
+            $api->get('me', 'Auth\LoginController@me')
+                ->name('api.auth.me');
         });
     });
 
