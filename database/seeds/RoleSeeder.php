4f1d9fd70a5c70b2c741cd37b764c052febd1b90
matical <maticalssh@gmail.com>
2017-12-19 22:20:27 +0800

    admin -> root

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -12,7 +12,7 @@ class RoleSeeder extends Seeder
     public function run()
     {
         $roles = [
-            'admin',
+            'root',
             'merchant',
             'user',
         ];
