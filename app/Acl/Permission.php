0f54fa321c116e7f3daf4eb8ab82046d8c11caca
Amatsuka Mao <maticalssh@gmail.com>
2017-12-19 14:36:12 +0000

    Apply fixes from StyleCI

--- app/Acl/Permission.php
+++ app/Acl/Permission.php
@@ -6,8 +6,6 @@ use Spatie\Permission\Models\Permission as BasePermission;
 
 /**
  * Base Permissions model. Inherits from Spatie's package.
- *
- * @package App\Acl
  */
 class Permission extends BasePermission
 {
