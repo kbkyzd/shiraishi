8ab7653dbeefde5e5ad679159ad9330796eae434
matical <maticalssh@gmail.com>
2017-12-28 13:47:09 +0800

    Add contact field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')
+                  ->unique();
             $table->string('password');
+            $table->string('contact')
+                  ->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
