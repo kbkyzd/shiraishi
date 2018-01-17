d0ab5f7828806f3214b6a6cd254ebc14cb388f40
matical <maticalssh@gmail.com>
2018-01-17 11:28:41 +0800

    Initial controller

---
+++ app/Api/Controllers/TwitterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace shiraishi\Api\Controllers;
+
+use shiraishi\Api\Controllers\BaseApiController as ApiController;
+
+class TwitterController extends ApiController
+{
+    public function index()
+    {
+        
+    }
+}
