a2340860f2cb58ee4106e21f56488acab18636bf
matical <maticalssh@gmail.com>
2018-02-01 21:25:31 +0800

    QrCode stubs

--- tsumugi/Foundation/QrCode.php
+++ tsumugi/Foundation/QrCode.php
@@ -2,6 +2,12 @@
 
 namespace tsumugi\Foundation;
 
+use SimpleSoftwareIO\QrCode\Facades\QrCode as SimpleQrCode;
+
 class QrCode
 {
+    public function generate($payload)
+    {
+        return SimpleQrCode::generate($payload);
+    }
 }
