cd182e1d6c86779732c9dbe010d31cb33ac86cac
matical <maticalssh@gmail.com>
2018-02-01 13:58:09 +0800

    Cast processed_at as date

--- app/Order.php
+++ app/Order.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $dates = [
+        'processed_at',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
