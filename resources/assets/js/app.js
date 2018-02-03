62bde44ccfd1abedda51d2a223adbd983549b554
matical <maticalssh@gmail.com>
2018-02-03 23:39:34 +0800

    Init materialize components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,12 +11,32 @@ const app = new Vue({
 
 (function($) {
     $(function() {
-        let mdDescription = new SimpleMDE({
-            element: $('#markdown-description')[0]
+        // let mdTerms = new SimpleMDE({
+        //     element: $('#markdown-terms')[0]
+        // });
+        $('select:not(.phpdebugbar-datasets-switcher)').material_select();
+
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 10, // Creates a dropdown of 15 years to control year,
+            today: 'Today',
+            clear: 'Clear',
+            close: 'Ok',
+            closeOnSelect: false, // Close upon selecting a date,
+            format: 'd mmmm, yyyy',
+            formatSubmit: 'yyyy-mm-dd',
+            hiddenName: true
         });
 
-        let mdTerms = new SimpleMDE({
-            element: $('#markdown-terms')[0]
+        $('.timepicker').pickatime({
+            default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+            fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+            twelvehour: false, // Use AM/PM or 24-hour format
+            donetext: 'OK', // text for done-button
+            cleartext: 'Clear', // text for clear-button
+            canceltext: 'Cancel', // Text for cancel-button
+            autoclose: false, // automatic close timepicker
+            ampmclickable: true // make AM PM clickable
         });
     });
 })(jQuery);
