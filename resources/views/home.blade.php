86cbabd097fc8f4bd45a766f965fedd623501dd2
matical <maticalssh@gmail.com>
2018-02-04 02:38:16 +0800

    Bring in instascan

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="header-shell">
-        <h4><i class="fa fa-user"></i> User Management</h4>
-        <span class="tags"><i class="fa fa-users"></i>0 Users</span>
-    </div>
-@endsection
